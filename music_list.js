71bfc0e0781e69b3afec95127e2258a155ba2312
yechi817 <kqhqpfg5x8@privaterelay.appleid.com>
2026-05-27 15:59:08 +0900

    기초 뮤직 플레이어
    기초 뮤직 플레이어 입니다.

---
+++ music_list.js
@@ -0,0 +1,8 @@
+const musicList = [
+  { name:"Alone", artist:"Color Out", audio:"Alone", img:"album1", total:"04:05" },
+  { name:"Mahidevran", artist:"Mahidevran rock band", audio:"Mahidevran-Maze_of_sorrow", img:"album2", total:"06:28" },
+  { name:"No Rest Or Endless Rest", artist:"Lisofv", audio:"No_Rest_Or_Endless_Rest", img:"album3", total:"04:33" },
+  { name:"Old Melody", artist:"TAB", audio:"Old_Money", img:"album4", total:"04:01" },
+  { name:"Tantalizing Youth", artist:"Social Square", audio:"Tantalizing_Youth", img:"album5", total:"04:12" },
+  { name:"The Deep", artist:"Anitek", audio:"The_Deep", img:"album6", total:"02:24" }
+]
